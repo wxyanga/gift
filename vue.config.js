2eb8c9737814444b4e72eda3d4a38f3152f63a93
wxyanga <25612926@qq.com>
2020-05-29 22:09:26 +0800

    编译的js用时间戳生成

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,16 @@ const isProduction = process.env.NODE_ENV === 'production'
 function resolve(dir) {
 	return path.join(__dirname, dir)
 }
+const Timestamp = new Date().getTime();
+process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
+	configureWebpack: { // webpack 配置
+		output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+			filename: `[name].${process.env.VUE_APP_Version}.${Timestamp}.js`,
+			chunkFilename: `[name].${process.env.VUE_APP_Version}.${Timestamp}.js`
+		},
+	},
 	// baseUrl: './',
 	productionSourceMap: false,
 	publicPath: isProduction ? '/static/kbao/' : '/',
